d9f4743ab774184d0538ab023f85d539281ed428
namdevsneha <snehanamdev40@gmail.com>
2024-10-04 14:31:24 +0530

    Add initial files for Home, Blog, Auth, Product, Profile, and Routes pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,24 @@
 import {BrowserRouter,Routes, Route} from "react-router-dom";
 
+import Home from "./pages/Home/Home";
+import Product from "./pages/Product/Product";
+import Blog from "./pages/Home/Blog";
+import FeeEyeTest from "./pages/Home/FreeEyeTest";
+import AboutUS from "./pages/Home/AboutUs";
 
 export default function App() {
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/product" element={<Product />} />
+      <Route path="/blog" element={<Blog />} />
+      <Route path="/free-eye-test" element={<FeeEyeTest />} />
+      <Route path="/about-us" element={<AboutUS />} />
 
-  return
-  (
-  <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
 
-      </Routes>
+    </Routes>
+
   </BrowserRouter>
-  );   
+  
 }
 
