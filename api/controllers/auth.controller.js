c61ccc4bab9dbe83a874812134043a503325a2ff
noddy0070 <utkarshsaxena@rediffmail.com>
2025-05-27 09:57:38 +0530

    updated cookie settings in Google callback to set SameSite attribute to 'None' for improved cross-site compatibility

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -128,7 +128,7 @@ export const googleCallback = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: true,
-      sameSite: 'Strict',
+      sameSite: 'None',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
