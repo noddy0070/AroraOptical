eef43f47086c1b39f11aa60e1ad9efcd3d27b81f
noddy0070 <utkarshsaxena@rediffmail.com>
2025-07-07 01:26:06 +0530

    changes done on logos

--- client/src/pages/Home/Sections/Brands.jsx
+++ client/src/pages/Home/Sections/Brands.jsx
@@ -8,25 +8,31 @@ import * as logos from "../../../assets/images/companyLogos";
 
 const logosData = [
     { src: logos.sf, alt: "SF",height:5.5, heightPhone:10 },
-    { src: logos.jimmychoo, alt: "Jimmy Choo",height:2.75, heightPhone:5 },
-    { src: logos.burberry, alt: "Burberry",height:4.875, heightPhone:9 },
-    { src: logos.bvlgari, alt: "Bvlgari",height:1.625, heightPhone:3 },
+    { src: logos.jimmychoo, alt: "Jimmy Choo",height:3.5, heightPhone:5 },
+    { src: logos.burberry, alt: "Burberry",height:5, heightPhone:9 },
     { src: logos.prada, alt: "Prada",height:8.125, heightPhone:12 },
     { src: logos.emporioarmani, alt: "Emporio Armani",height:2, heightPhone:2.50 },
     { src: logos.rayban, alt: "Ray-Ban",height:5.4375, heightPhone:10 },
-    { src: logos.ess, alt: "ESS",height:6.25, heightPhone:11.25 },
     { src: logos.miumiu, alt: "Miu Miu",height:2.625, heightPhone:4.75 },
-    { src: logos.swarovski, alt: "Swarovski",height:2.125, heightPhone:3.75 },
-    { src: logos.dolcegabbana, alt: "Dolce & Gabbana",height:1.625, heightPhone:3 },
-    { src: logos.giorgioarmani, alt: "Giorgio Armani",height:2.375, heightPhone:4.25 },
-    { src: logos.michaelkors, alt: "Michael Kors",height:4.5, heightPhone:8.25 },
-    { src: logos.ralphlauren, alt: "Ralph Lauren",height: 2.25, heightPhone:4 },
+    { src: logos.swarovski, alt: "Swarovski",height:3, heightPhone:3.75 },
+    { src: logos.dolcegabbana, alt: "Dolce & Gabbana",height:3, heightPhone:3 },
+    { src: logos.michaelkors, alt: "Michael Kors",height:3, heightPhone:8.25 },
     { src: logos.prada2, alt: "Prada 2",height:2.5, heightPhone:6.5 },
-    { src: logos.dakley, alt: "Dakley",height: 5.125, heightPhone:9.4},
-    { src: logos.tiffany, alt: "Tiffany" ,height:2.5, heightPhone:4.5},
-    { src: logos.toryburch, alt: "Tory Burch",height:3.0625, heightPhone:5.5 },
+    { src: logos.oakley, alt: "Oakley",height: 5.125, heightPhone:9.4},
     { src: logos.versace, alt: "Versace",height:5.75, heightPhone:10.5 },
     { src: logos.vogue, alt: "Vogue", height:5.625, heightPhone:10.25 },
+    { src: logos.coach, alt:'coach', height:1.875, heightPhone:3.5},
+    { src: logos.fossil,alt:'fossil', height:1.875, heightPhone:5.5},
+    { src: logos.mauijim, alt:'mauijim',height: 5.75, heightPhone:10.5},
+    { src: logos.calvinklein,alt:'calvinklein', height:2.75, heightPhone:5},
+    {src: logos.pierrecardin,alt:'pierrecardin', height:5.25, heightPhone:9.5},
+    {src: logos.levis,alt:'levis', height:5.625, heightPhone:10.25},
+    {src: logos.anahickmann,alt:'anahickmann', height:7, heightPhone:12.75},
+    {src: logos.ditalancier,alt:'ditalancier', height:2.5, heightPhone:4},
+    {src: logos.montblanc,alt:'montblanc', height:3.25, heightPhone:6},
+    {src: logos.hugo,alt:'hugo', height:6.625, heightPhone:12},
+    {src: logos.boss,alt:'boss', height:7.875, heightPhone:14.5},
+    {src: logos.armaniexchange,alt:'armaniexchange', height:9, heightPhone:16.5}
   ];
 
 export default function Brands() {
@@ -59,8 +65,8 @@ export default function Brands() {
             slidesToScroll: 1,
             centerMode: true,
             autoplay: true,
-            speed: 1000,
-            autoplaySpeed: 1000,
+            speed: 1500,
+            autoplaySpeed: 1500,
             cssEase: "linear",
             variableWidth: true,
             pauseOnHover: false,       // Prevent autoplay from stopping on hover
@@ -73,7 +79,7 @@ export default function Brands() {
            
         {logosData.map((logo, index) => (
         <div key={index} className="flex relative justify-center items-center">
-          <img src={logo.src} alt={logo.alt} className="max-h-full object-contain" style={screenWidth>768?{height:`${logo.height}vw`}:{height:`${logo.heightPhone}vw`}} />
+          <img src={logo.src} alt={logo.alt} className="max-h-full object-contain " style={screenWidth>768?{height:`${logo.height}vw`}:{height:`${logo.heightPhone}vw`}} />
             
         </div>
       ))}
